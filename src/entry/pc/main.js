9103ba84ac683da67fbd8ed6296408d46c73c3d7
yangguanyong <453493671@qq.com>
2020-01-20 11:23:36 +0800

    增加reset.scss

--- src/entry/pc/main.js
+++ src/entry/pc/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import components from '@/components/index'
+import '@/assets/reset.scss'
 
 Vue.use(ElementUI)
 Vue.use(components)
